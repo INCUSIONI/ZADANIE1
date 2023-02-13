7cc9c26a42666c9db5aa581afa394486ddb30540
INCUSIONI <122853731+INCUSIONI@users.noreply.github.com>
2023-02-13 18:57:32 +0300

    Add files via upload

---
+++ 15/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace MetanitApp
+{
+    public class Class1
+    {
+    }
+}
---
+++ 15/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+namespace MetanitApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            usersList.Items.Remove("Sam");      // удаляем элемент "Sam"
+            usersList.Items.RemoveAt(1);        // удаляем второй элемент
+            usersList.Items.Add("Kate");        // Добавляем элемент "Kate"
+            usersList.Items.Insert(0, "Mike"); // Вставляем элемент "Mike" на первое место в списке
+        }
+    }
+}
