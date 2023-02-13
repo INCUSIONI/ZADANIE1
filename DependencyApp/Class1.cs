9c111e106f927c7a360d210936d91b8a20d3afab
INCUSIONI <122853731+INCUSIONI@users.noreply.github.com>
2023-02-13 22:58:07 +0300

    Add files via upload

---
+++ DependencyApp/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace DependencyApp
+{
+    public class Class1
+    {
+    }
+}
---
+++ DependencyApp/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DependencyApp
+{
+
+
+
+
+
+    public class Phone : DependencyObject
+    {
+        public static readonly DependencyProperty TitleProperty;
+        public static readonly DependencyProperty PriceProperty;
+
+        static Phone()
+        {
+            TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(Phone));
+            PriceProperty = DependencyProperty.Register("Price", typeof(int), typeof(Phone));
+        }
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public int Price
+        {
+            get { return (int)GetValue(PriceProperty); }
+            set { SetValue(PriceProperty, value); }
+        }
+    }
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
