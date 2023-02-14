416998ef974209e1f1be23ae0224fb531ba0e4da
INCUSIONI <122853731+INCUSIONI@users.noreply.github.com>
2023-02-14 11:57:12 +0400

    Add files via upload

---
+++ Элементы управления содержимым 8/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace ControlsApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+           
+        }
+    }
+}
+
